d4e47e057c2a1c0a4956179472b2fa526222eadb
Foad Botan <foadbotan@gmail.com>
2023-10-14 14:54:02 +1100

    Style TimeZonesLIst

--- src/components/TimeZonesList.tsx
+++ src/components/TimeZonesList.tsx
@@ -26,6 +26,11 @@ const timeZones: TimeZone[] = [
     offset: 10,
     people: ['Chandler', 'Ross'],
   },
+  {
+    name: 'Tokyo',
+    offset: -4,
+    people: ['Gunther', 'Janice'],
+  },
 ];
 
 const MINUTES_IN_HOUR = 60;
@@ -65,88 +70,91 @@ export default function TimeZonesList() {
   const [selectedTime, setSelectedTime] = useState(MINUTES_IN_DAY / 2);
 
   return (
-    <div className="relative py-6">
+    <>
       <p className="text-xl">{formatTime(selectedTime)}</p>
-      <input
-        type="range"
-        className="absolute inset-0 z-20 bg-transparent appearance-none cursor-pointer h-full w-full"
-        min={0}
-        max={MINUTES_IN_DAY}
-        step={15}
-        value={selectedTime}
-        onChange={(event) => setSelectedTime(Number(event.target.value))}
-      />
-      <div
-        className="absolute top-0 bottom-0 z-10 w-1 bg-red-500"
-        style={{
-          left: `${(selectedTime / MINUTES_IN_DAY) * 100}%`,
-        }}
-      />
-      <ul>
-        {timeZones.map((timeZone) => {
-          let workStart = getWorkStart(timeZone);
-          let workEnd = getWorkEnd(timeZone);
-          workStart = workStart < 0 ? 0 : workStart;
-          workEnd = workEnd > MINUTES_IN_DAY ? MINUTES_IN_DAY : workEnd;
-
-          return (
-            <li key={timeZone.name} className="pb-6">
-              <div className="flex items-end justify-between pb-2">
-                <div className="">
-                  <p className="text-sm font-semibold leading-6 text-neutral-900">
-                    {timeZone.name}
-                  </p>
-                  <p className="mt-1 text-xs leading-5 text-neutral-500">
-                    {`UTC ${timeZone.offset < 0 ? '' : '+'}${timeZone.offset}`}
-                  </p>
+      <div className="relative">
+        <input
+          type="range"
+          className="absolute inset-0 z-20 bg-transparent appearance-none cursor-pointer h-full w-full"
+          min={0}
+          max={MINUTES_IN_DAY}
+          step={15}
+          value={selectedTime}
+          onChange={(event) => setSelectedTime(Number(event.target.value))}
+        />
+        <div
+          className="absolute top-0 bottom-0 z-10 w-1 bg-red-500"
+          style={{
+            left: `${(selectedTime / MINUTES_IN_DAY) * 100}%`,
+          }}
+        />
+        <ul className="border rounded-xl p-4 pb-10 shadow-lg my-6 space-y-6">
+          {timeZones.map((timeZone) => {
+            let workStart = getWorkStart(timeZone);
+            let workEnd = getWorkEnd(timeZone);
+            workStart = workStart < 0 ? 0 : workStart;
+            workEnd = workEnd > MINUTES_IN_DAY ? MINUTES_IN_DAY : workEnd;
+            return (
+              <li key={timeZone.name}>
+                <div className="flex items-end justify-between pb-2">
+                  <div className="">
+                    <p className="text-sm font-semibold leading-6 text-neutral-900">
+                      {timeZone.name}
+                    </p>
+                    <p className="mt-1 text-xs leading-5 text-neutral-500">
+                      {`UTC ${timeZone.offset < 0 ? '' : '+'}${timeZone.offset}`}
+                    </p>
+                  </div>
+                  <div
+                    className={cn('flex gap-2', {
+                      '[&_img]:bg-green-500': inWorkingHours(selectedTime, timeZone),
+                    })}
+                  >
+                    {timeZone.people.map((person) => (
+                      <div key={person} className="flex flex-col items-center ">
+                        {/* eslint-disable-next-line @next/next/no-img-element */}
+                        <img
+                          className={cn(
+                            'inline-block w-12 h-12 rounded-full bg-neutral-200'
+                          )}
+                          src={avatarURL(person)}
+                          alt={person}
+                          width="48"
+                          height="48"
+                        />
+                        <span className="max-w-[12ch] text-xs truncate text-neutral-600">
+                          {person}
+                        </span>
+                      </div>
+                    ))}
+                  </div>
                 </div>
                 <div
-                  className={cn('flex gap-2', {
-                    '[&_img]:bg-green-500': inWorkingHours(selectedTime, timeZone),
-                  })}
+                  className="grid w-full overflow-hidden rounded bg-neutral-200"
+                  style={{
+                    gridTemplateColumns: `repeat(${TIME_BLOCK}, 1fr)`,
+                  }}
                 >
-                  {timeZone.people.map((person) => (
-                    <div key={person} className="flex flex-col items-center ">
-                      {/* eslint-disable-next-line @next/next/no-img-element */}
-                      <img
-                        className={cn(
-                          'inline-block w-12 h-12 rounded-full bg-neutral-200'
-                        )}
-                        src={avatarURL(person)}
-                        alt={person}
-                        width="48"
-                        height="48"
-                      />
-                      <span className="max-w-[12ch] text-xs truncate text-neutral-600">
-                        {person}
-                      </span>
-                    </div>
-                  ))}
+                  {Array.from({ length: TIME_BLOCK }).map((_, index) => {
+                    const time = index * 15;
+                    const isWorkTime = time >= workStart && time <= workEnd;
+                    return (
+                      <div
+                        key={time}
+                        className={cn('h-6', {
+                          'bg-blue-400': isWorkTime,
+                          'bg-blue-500':
+                            isWorkTime && inWorkingHours(selectedTime, timeZone),
+                        })}
+                      ></div>
+                    );
+                  })}
                 </div>
-              </div>
-              <div
-                className="grid w-full overflow-hidden rounded bg-neutral-200"
-                style={{
-                  gridTemplateColumns: `repeat(${TIME_BLOCK}, 1fr)`,
-                }}
-              >
-                {Array.from({ length: TIME_BLOCK }).map((_, index) => {
-                  const time = index * 15;
-                  const isWorkTime = time >= workStart && time <= workEnd;
-                  return (
-                    <div
-                      key={time}
-                      className={cn('flex items-center justify-center h-6', {
-                        'bg-blue-400': isWorkTime,
-                      })}
-                    ></div>
-                  );
-                })}
-              </div>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </>
   );
 }
