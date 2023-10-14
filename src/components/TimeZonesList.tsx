5889b06b4e561d8ab7cf15902d34e52efabff609
Foad Botan <foadbotan@gmail.com>
2023-10-14 14:33:15 +1100

    Highlight users who available at the selected time

--- src/components/TimeZonesList.tsx
+++ src/components/TimeZonesList.tsx
@@ -1,8 +1,16 @@
 'use client';
 
+import { cn } from '@/lib/utils';
+import { log, time } from 'console';
 import { useState } from 'react';
 
-const timeZones = [
+type TimeZone = {
+  name: string;
+  offset: number;
+  people: string[];
+};
+
+const timeZones: TimeZone[] = [
   {
     name: 'London',
     offset: 1,
@@ -23,19 +31,35 @@ const timeZones = [
 const MINUTES_IN_HOUR = 60;
 const HOURS_IN_DAY = 24;
 const MINUTES_IN_DAY = MINUTES_IN_HOUR * HOURS_IN_DAY;
+const TIME_BLOCK = MINUTES_IN_DAY / 15;
 
 const avatarURL = (seed: string): string =>
   `https://api.dicebear.com/7.x/lorelei/svg?seed=${seed}`;
 
-const formatTime = (time: number): string => {
-  const hours = Math.floor(time / MINUTES_IN_HOUR);
+function formatTime(time: number): string {
   const minutes = time % MINUTES_IN_HOUR;
+  const hours = (time - minutes) / MINUTES_IN_HOUR;
 
   const hoursString = String(hours).padStart(2, '0');
   const minutesString = String(minutes).padStart(2, '0');
 
   return `${hoursString}:${minutesString}`;
-};
+}
+
+function inWorkingHours(eventTime: number, timeZone: TimeZone): boolean {
+  const workStart = getWorkStart(timeZone);
+  const workEnd = getWorkEnd(timeZone);
+
+  return eventTime >= workStart && eventTime <= workEnd;
+}
+
+function getWorkStart(timeZone: TimeZone): number {
+  return timeZone.offset * MINUTES_IN_HOUR;
+}
+
+function getWorkEnd(timeZone: TimeZone): number {
+  return MINUTES_IN_HOUR * 12 + getWorkStart(timeZone);
+}
 
 export default function TimeZonesList() {
   const [selectedTime, setSelectedTime] = useState(MINUTES_IN_DAY / 2);
@@ -59,49 +83,69 @@ export default function TimeZonesList() {
         }}
       />
       <ul>
-        {timeZones.map((timeZone) => (
-          <li key={timeZone.name} className="pb-6">
-            <div className="flex items-end justify-between pb-2">
-              <div className="">
-                <p className="text-sm font-semibold leading-6 text-neutral-900">
-                  {timeZone.name}
-                </p>
-                <p className="mt-1 text-xs leading-5 text-neutral-500">
-                  {`UTC ${timeZone.offset < 0 ? '' : '+'}${timeZone.offset}`}
-                </p>
+        {timeZones.map((timeZone) => {
+          let workStart = getWorkStart(timeZone);
+          let workEnd = getWorkEnd(timeZone);
+          workStart = workStart < 0 ? 0 : workStart;
+          workEnd = workEnd > MINUTES_IN_DAY ? MINUTES_IN_DAY : workEnd;
+
+          return (
+            <li key={timeZone.name} className="pb-6">
+              <div className="flex items-end justify-between pb-2">
+                <div className="">
+                  <p className="text-sm font-semibold leading-6 text-neutral-900">
+                    {timeZone.name}
+                  </p>
+                  <p className="mt-1 text-xs leading-5 text-neutral-500">
+                    {`UTC ${timeZone.offset < 0 ? '' : '+'}${timeZone.offset}`}
+                  </p>
+                </div>
+                <div
+                  className={cn('flex gap-2', {
+                    '[&_img]:bg-green-500': inWorkingHours(selectedTime, timeZone),
+                  })}
+                >
+                  {timeZone.people.map((person) => (
+                    <div key={person} className="flex flex-col items-center ">
+                      {/* eslint-disable-next-line @next/next/no-img-element */}
+                      <img
+                        className={cn(
+                          'inline-block w-12 h-12 rounded-full bg-neutral-200'
+                        )}
+                        src={avatarURL(person)}
+                        alt={person}
+                        width="48"
+                        height="48"
+                      />
+                      <span className="max-w-[12ch] text-xs truncate text-neutral-600">
+                        {person}
+                      </span>
+                    </div>
+                  ))}
+                </div>
               </div>
-              <div className="flex gap-2">
-                {timeZone.people.map((person) => (
-                  <div key={person} className="flex flex-col items-center ">
-                    {/* eslint-disable-next-line @next/next/no-img-element */}
-                    <img
-                      className="inline-block w-12 h-12 rounded-full bg-neutral-200"
-                      src={avatarURL(person)}
-                      alt={person}
-                      width="48"
-                      height="48"
-                    />
-                    <span className="max-w-[12ch] text-xs truncate text-neutral-600">
-                      {person}
-                    </span>
-                  </div>
-                ))}
-              </div>
-            </div>
-            <div className="flex w-full overflow-hidden rounded bg-neutral-200">
               <div
-                className="flex w-1/2 overflow-hidden rounded"
+                className="grid w-full overflow-hidden rounded bg-neutral-200"
                 style={{
-                  transform: `translateX(${(timeZone.offset / 24) * 100}%)`,
+                  gridTemplateColumns: `repeat(${TIME_BLOCK}, 1fr)`,
                 }}
               >
-                <div className="w-1/12 bg-blue-300 " />
-                <div className="flex items-center justify-center w-full h-6 bg-blue-400"></div>
-                <div className="w-2/12 bg-blue-300 " />
+                {Array.from({ length: TIME_BLOCK }).map((_, index) => {
+                  const time = index * 15;
+                  const isWorkTime = time >= workStart && time <= workEnd;
+                  return (
+                    <div
+                      key={time}
+                      className={cn('flex items-center justify-center h-6', {
+                        'bg-blue-400': isWorkTime,
+                      })}
+                    ></div>
+                  );
+                })}
               </div>
-            </div>
-          </li>
-        ))}
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
