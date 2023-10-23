7c151f282e078b16290f66f187d40dea88095a4b
Foad Botan <foadbotan@gmail.com>
2023-10-23 16:46:10 +1100

    Simplify form and use react-select

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 import TimeZonesList from '@/components/TimeZonesList';
-import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="container py-10">
-      <h1 className="text-3xl font-bold leading-tight tracking-tight text-gray-900 text-center m-4">
+    <main className="container py-6">
+      <h1 className="text-3xl font-bold leading-tight tracking-tight text-gray-900 text-center m-10">
         Team Zones
       </h1>
       <TimeZonesList />
--- src/components/AddPersonForm.tsx
+++ src/components/AddPersonForm.tsx
@@ -1,70 +1,80 @@
-import { TIME_ZONES, TIME_ZONE_REGIONS, formatUtcOffset } from '@/lib/utils';
 import { Person } from '@/types';
+import { useState } from 'react';
+import Select from 'react-select';
 
 type Props = {
   addPerson: (person: Person) => void;
 };
 
+type TimeZoneOption = {
+  value: string;
+  label: string;
+};
+
+const timeZoneOptions: TimeZoneOption[] = Intl.supportedValuesOf('timeZone').map(
+  (timeZone) => ({
+    value: timeZone,
+    label: timeZone,
+  }),
+);
+
 export default function AddPersonForm({ addPerson }: Props) {
+  const [name, setName] = useState('');
+  const [timeZone, setTimeZone] = useState<TimeZoneOption | null>(null);
+
   function onSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
+    if (!timeZone || !name) return;
 
-    const form = event.currentTarget;
-    const formData = new FormData(form);
-    const name = formData.get('name');
-    const timeZone = formData.get('timeZone');
     const person: Person = {
-      name: String(name),
-      timeZone: String(timeZone),
+      name: name.trim(),
+      timeZone: timeZone.value,
       isSelected: true,
     };
 
-    if (!timeZone) return;
-
     addPerson(person);
-    form.reset();
+
+    setName('');
+    setTimeZone(null);
   }
+
   return (
     <form
       onSubmit={onSubmit}
-      className="mt-20 space-y-4 flex flex-col max-w-sm border rounded-xl p-6 shadow-md"
+      className="mt-20 flex max-w-sm flex-col space-y-4 rounded-xl border p-6 shadow-md"
     >
       <h3 className="text-xl font-medium text-gray-900">Add a team member</h3>
 
-      <div className="flex flex-col gap-1 flex-1">
+      <div className="flex flex-1 flex-col gap-1">
         <label htmlFor="name">Name</label>
         <input
           id="name"
           name="name"
-          className="px-4 py-2 border rounded-md shadow-sm focus:ring-neutral-500 focus:border-neutral-500 sm:text-sm"
+          className="rounded-sm border border-neutral-300 px-3 py-1.5 placeholder:text-neutral-500"
           placeholder="John Doe"
+          required
+          minLength={2}
+          maxLength={50}
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
+      </div>
+      <div className="flex flex-1 flex-col gap-1">
+        <label htmlFor="timeZone">Time Zone</label>
+
+        <Select
+          options={timeZoneOptions}
+          onChange={(option) => setTimeZone(option)}
+          value={timeZone}
+          className="z-30"
+          isClearable
+          placeholder="Select a time zone"
           required
         />
       </div>
-      <div className="flex flex-col flex-1 gap-1">
-        <label htmlFor="timeZone">Time Zone</label>
-        <select
-          id="timeZone"
-          name="timeZone"
-          className="px-4 py-2 border rounded-md shadow-sm focus:ring-neutral-500 focus:border-neutral-500 sm:text-sm"
-          required
-        >
-          <option value="">Select a time zone</option>
-
-          {Object.keys(TIME_ZONE_REGIONS).map((region) => (
-            <optgroup key={region} label={region}>
-              {TIME_ZONE_REGIONS[region].map(({ timeZone, city, formattedOffset }) => (
-                <option key={timeZone} value={timeZone}>
-                  {city} — {formattedOffset}
-                </option>
-              ))}
-            </optgroup>
-          ))}
-        </select>
-      </div>
       <button
         type="submit"
-        className="px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-blue-500 border border-transparent rounded-md shadow-sm hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+        className="rounded-md bg-blue-500 px-4 py-2 text-sm font-medium leading-5 text-white shadow-sm  hover:bg-blue-600 "
       >
         Add
       </button>
